893b54d542fb177d0b7baf44e81a05fe9a3ecb27
Richard Sivak <sivak.r101@gmail.com>
2017-01-21 13:30:22 -0500

    Finished Student Side. Added admin files

--- CourseRegistration/Controllers/HomeController.cs
+++ CourseRegistration/Controllers/HomeController.cs
@@ -3,28 +3,125 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CourseRegistration.Models;
 
 namespace CourseRegistration.Controllers
 {
     public class HomeController : Controller
     {
+        CourseRegistrationEntities dbContext = new CourseRegistrationEntities();
+        #region Student
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public JsonResult GetCourses()
         {
-            ViewBag.Message = "Your application description page.";
+            var courses = from course in dbContext.Courses
+                          select new
+                          {
+                              Id = course.Id,
+                              CourseId = course.CourseId,
+                              Title = course.Title,
+                              Credits = course.Credits,
+                              Capacity = course.Capacity,
+                              Description = course.Description,
+                              RegisteredCount = course.Students.ToList().Count()
+                          };
+            return Json(courses, JsonRequestBehavior.AllowGet);
+        }
 
+        public JsonResult Login(int studentId)
+        {
+            try
+            {
+                var thisStudent = (from student in dbContext.Students
+                                   select new
+                                   {
+                                       Id = student.Id,
+                                       StudentId = student.StudentId,
+                                       FirstName = student.FirstName,
+                                       LastName = student.LastName,
+                                       Courses = from course in student.Courses.ToList()
+                                                 select new
+                                                 {
+                                                     Id = course.Id,
+                                                     CourseId = course.CourseId,
+                                                     Title = course.Title,
+                                                     Credits = course.Credits,
+                                                     Capacity = course.Capacity,
+                                                     Description = course.Description
+                                                 }
+                                   }).First(m => m.StudentId == studentId);
+                return Json(thisStudent, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                Response.StatusCode = 418;
+                Response.StatusDescription = "I'm a teapot";
+                return Json(new { Error = "That doesn't look like a valid User ID" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult Register(int courseId, int studentId)
+        {
+            try
+            {
+                var stud = dbContext.Students.First(m => m.Id == studentId);
+                var crs = dbContext.Courses.First(m => m.Id == courseId);
+                if (crs.Capacity - crs.Students.Count() > 0)
+                {
+                    stud.Courses.Add(crs);
+                    dbContext.SaveChanges();
+                    return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    Response.StatusCode = 418;
+                    Response.StatusDescription = "I'm a teapot!";
+                    return Json(new { Errors = "Unfortunately, the course is full!" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception)
+            {
+                return Json(new { success = false }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult Drop(int courseId, int studentId)
+        {
+            try
+            {
+                var stud = dbContext.Students.First(m => m.Id == studentId);
+                var crs = dbContext.Courses.First(m => m.Id == courseId);
+                stud.Courses.Remove(crs);
+                dbContext.SaveChanges();
+                return Json(new { RegisteredCount = crs.Students.ToList().Count(), Capacity = crs.Capacity, CourseId = crs.CourseId, Credits = crs.Credits, Description = crs.Description, Id = crs.Id, Title = crs.Title }
+                            , JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                return Json(new { Errors = "The course wasn't successfully dropped, refresh to try again." }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        #endregion
+
+        #region Admin
+        public ActionResult Admin()
+        {
             return View();
         }
 
-        public ActionResult Contact()
+        #endregion
+        public new void Dispose()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            dbContext.Dispose();
+            // Dispose of unmanaged resources.
+            Dispose(true);
+            // Suppress finalization.
+            GC.SuppressFinalize(this);
         }
     }
 }
---
+++ CourseRegistration/Scripts/Student.js
@@ -0,0 +1,123 @@
+﻿(function () {
+    var app = angular.module("StudentApp", []);
+
+    app.controller("StudentController", ['$http', function ($http) {
+        var self = this;
+        this.LoggedIn = false;
+        this.UserId = '';
+        this.User = {};
+        this.Errors = [];
+
+        this.Courses = [];
+
+        $http.get('/Home/GetCourses').then(
+            function (retVal) {
+                self.Courses = retVal.data;
+                self.Courses = self.Courses.filter(function (el) {
+                    return el.Capacity - el.RegisteredCount > 0;
+                });
+            }, function (retVal) {
+                self.Errors.push({ key: "Courses", value: "An error occurred loading the courses, please try again." });
+            }
+        );
+
+        this.Login = function (url) {
+            self.User = {};
+            self.Errors = [];
+            if (self.UserId && self.Password) {
+                $http.get(url + '?studentId=' + self.UserId).then(
+                    function (retVal) {
+                        self.User = retVal.data;
+                        if (!self.User.Courses) {
+                            self.User.Courses = [];
+                        } else {
+                            for (var i = 0; i < self.User.Courses.length; i++) {
+                                self.Courses = self.Courses.filter(function (el) {
+                                    return el.Id !== self.User.Courses[i].Id;
+                                });
+                            }
+                        }
+                        self.User.SelectedCourses = [];
+                        self.LoggedIn = true;
+                    },
+                    function (retVal) {
+                        console.log(retVal);
+                        self.Errors.push({ key: "User ID", value: "There was no user with that User ID" })
+                    }
+                );
+            } else {
+                self.Errors.push({ key: "User ID or Password", value: "You must provide a valid User ID and a password. The password for this application can be anything" });
+            }
+        }
+        this.Logout = function () {
+            self.User = {};
+            self.LoggedIn = false;
+            self.Errors = [];
+            return;
+        }
+
+        this.AddToSelected = function (index) {
+            self.User.SelectedCourses.push(self.Courses[index]);
+            self.Courses.splice(index, 1);
+        }
+
+        this.Register = function (index) {
+            self.Errors = [];
+            $http.post('/Home/Register', { courseId: self.User.SelectedCourses[index].Id, studentId: self.User.Id })
+            .then(function () {
+                self.User.Courses.push(self.User.SelectedCourses[index]);
+                self.User.SelectedCourses.splice(index, 1);
+            }, function (retval) {
+                self.Errors.push({ key: 'Registering for that Course', value: retval.data.Errors });
+            });
+        }
+        this.Drop = function (index) {
+            if (confirm('Are you sure you would like to drop this course?')) {
+                self.Errors = [];
+                $http.post('/Home/Drop', { courseId: self.User.Courses[index].Id, studentId: self.User.Id })
+                .then(function (retval) {
+                    self.Courses.push(retval.data);
+                    self.Courses.sort(function (a, b) { return a.CourseId.localeCompare(b.CourseId); })
+                    self.User.Courses.splice(index, 1);
+                }, function (retval) {
+                    self.Errors.push({ key: 'Dropping Course', value: retval.data.Errors });
+                });
+            }
+        }
+        this.Remove = function (index) {
+            self.Courses.push(self.User.SelectedCourses[index]);
+            self.Courses.sort(function (a, b) { return a.CourseId.localeCompare(b.CourseId); })
+            self.User.SelectedCourses.splice(index, 1);
+
+        }
+
+        app.directive('focusOnShow', function ($timeout) {
+            return {
+                restrict: 'A',
+                link: function ($scope, $element, $attr) {
+                    if ($attr.ngShow) {
+                        $scope.$watch($attr.ngShow, function (newValue) {
+                            if (newValue) {
+                                $timeout(function () {
+                                    $element[0].focus();
+                                }, 0);
+                            }
+                        })
+                    }
+                    if ($attr.ngHide) {
+                        $scope.$watch($attr.ngHide, function (newValue) {
+                            if (!newValue) {
+                                $timeout(function () {
+                                    $element[0].focus();
+                                }, 0);
+                            }
+                        })
+                    }
+
+                }
+            };
+        })
+
+
+    }]);
+})();
